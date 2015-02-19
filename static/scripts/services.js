e2915d9d6fda2c7dd35595da12dfe4a6da7f2290
Sunil Syal <sunil.syal@gmail.com>
2015-02-19 11:39:42 +0530

    Added End Date in all tasks
    Automated the date display. Enter start date of the project in JSON and
    mention total days for an activity. It will automatically display the
    end date.

--- static/scripts/services.js
+++ static/scripts/services.js
@@ -9,6 +9,30 @@ angular.module('tradowApp')
         return objJSON;
     }).factory('projDetailsService', ['LoadService', function(LoadService) {
         var projObj = {};
+
+        function fnAddEndDateField (){
+            var startDate, len;
+            len = projObj.INFO.length;
+            startDate = Date.parse(projObj.STARTDATE);
+
+            var dateString = projObj.STARTDATE
+            var startDate = new Date(dateString);
+            startDate = new Date((startDate.setDate(startDate.getDate() - 1)))
+            
+            for(var i=0;i<len;i++){
+                var objRef = projObj.INFO[i];
+                var workingDays = Number(objRef.DAYS);
+                while(workingDays > 0){
+                    startDate = new Date((startDate.setDate(startDate.getDate() + 1)))
+                    if (startDate.getDay() == 0 || startDate.getDay() == 6){
+                        continue;
+                    }
+                    workingDays--;
+                }
+                
+                objRef.ENDDATE = startDate.toDateString();
+            } 
+        }
         return {
             getUsersList: function() {
                 return projObj;
@@ -17,6 +41,7 @@ angular.module('tradowApp')
                 //A parameter (project ID) will be paseed in case we are looking for users for selected project.
                 LoadService.loadFile("JSON/projectDetails.json").then(function(res) {
                     projObj = res.data.SHADOW;
+                    fnAddEndDateField();
                 }, function(err) {
                     //AlertService.set(err.data.msg);
                 });
